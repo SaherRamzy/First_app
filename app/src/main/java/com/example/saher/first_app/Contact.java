3a29c33c1459d33a06c1ce2853a9672b66643b6e
saherramzy <eng.saher9@gmail.com>
2015-12-09 16:16:23 +0200

    test

---
+++ app/src/main/java/com/example/saher/first_app/Contact.java
@@ -0,0 +1,9 @@
+package com.example.saher.first_app;
+
+
+public class Contact {
+    public String first_name;
+    public String second_name;
+    public String mobile;
+    public  String mobile2;
+}
